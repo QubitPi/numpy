d075595992fc18fdd1656f74be4649d55d5d64db
QubitPi <jack20220723@gmail.com>
2024-08-08 05:01:29 +0000

    deploy: f89a1ff7e213d4d9346467db32d9ba133129ad90

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '2.2.0.dev0+git20240808.496481e',
+    VERSION: '2.2.0.dev0+git20240808.f89a1ff',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
